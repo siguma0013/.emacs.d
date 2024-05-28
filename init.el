c188967e80d7a7d662b1f81f9573c54f406f33af
siguma0013 <siguma.lutetium.999@gmail.com>
2024-05-29 07:00:09 +0900

    update:不定期修正

--- init.el
+++ init.el
@@ -1,53 +1,14 @@
-;; スタートップメッセージを非表示
-(setq inhibit-startup-screen t)
 ;; Emacsからの質問をy/nで回答する
 (set 'yes-or-no-p 'y-or-n-p)
 
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
 
-;; show-paren-mode：対応する括弧を強調して表示する
-(show-paren-mode t)
-(setq show-paren-style 'expression)	;括弧内も強調:expression
-
-;; ミニバッファの履歴を保存する
-(savehist-mode t)
-(setq history-length 10000)		;履歴数
-
-;; バックアップファイルを作らない
-(setq make-backup-files nil)
-;; オートセーブファイルを作らない
-(setq auto-save-default nil)
-
-;; 選択範囲に色をつける
-(transient-mark-mode t)
-
 ;; コメントアウトのスタイル
 (setq comment-style 'extra-line)
 
-;; カーソルの点滅をやめる
-(blink-cursor-mode 0)
-
-;; タブをスペースで表現
-(setq-default indent-tabs-mode nil)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; フレーム関連
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 起動時のフレーム設定
-(load-theme 'tango-dark)
-
-;; モードラインに何文字目かも表示
-(column-number-mode t)
-;; フリンジ(左)に行数を表示する
-(global-display-line-numbers-mode t)
-
-;; ツールバー非表示
-(tool-bar-mode 0)
-;; メニューバー非表示
-(menu-bar-mode 0)
-;; スクロールバー非表示
-(scroll-bar-mode 0)
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(require 'alpha-command)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; フォント設定
@@ -56,36 +17,6 @@
 
 ;;1234;;1234;;1234;;1234;;1234;;
 ;;漢字;;漢字;;漢字;;漢字;;漢字;;
-
-;; 無駄なスペースの削除
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ediff
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; コントロール用のバッファを同一フレーム内に表示
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-;; diffのバッファを上下ではなく左右に並べる
-(setq ediff-split-window-function 'split-window-horizontally)
-
-(put 'dired-find-alternate-file 'disabled nil)
-
-;; "C-h"をバックスペースにする
-(keyboard-translate ?\C-h ?\C-?)
-
-(global-set-key (kbd "C-x C-b") 'bs-show)
-
-(when (require 'recentf nil t)
-  (setq recentf-max-saved-items 2000)
-  (recentf-mode 1)
-  )
-
-(setq-default header-line-format 'buffer-file-name)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; package
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when (require 'package nil t)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize)
@@ -93,113 +24,193 @@
   (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
   (when (file-exists-p custom-file)
     (load custom-file))
+
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
   )
 
+(leaf *builtin-ui
+  :custom ((line-number-mode . nil)     ; mode-lineに行番号
+           (column-number-mode . nil)   ; mode-lineに列番号
+
+           (custom-enabled-themes . '(tango-dark)) ; テーマ
+           (blink-cursor-mode . nil)               ; カーソル点滅
+           (global-display-line-numbers-mode . t)  ; 左端に行番号
+           ))
+
+(leaf *builtin-feature
+  :custom ((auto-save-default . nil)    ; 自動保存
+           (electric-pair-mode . t)     ; 引用符・括弧補完
+           (indent-tabs-mode . nil)     ; インデントをスペースで表現
+           (inhibit-startup-screen . t)
+           (make-backup-files . nil)    ; バックアップファイルの作成
+           (transient-mark-mode . t)    ; 選択範囲のハイライト
+           )
+  :config
+  (leaf *delete-trailing-whitespace
+    :hook ((before-save-hook . delete-trailing-whitespace)
+           ))
+
+  (leaf *recentf-mode
+    :custom ((recentf-max-saved-items . 2000)
+             (recentf-mode . t)
+             ))
+
+  (leaf *savehist-mode
+    :custom ((history-length . 1000)
+             (savehist-additional-variables . '(kill-ring))
+             (savehist-mode . t)
+             ))
+
+  (leaf *show-paren-mode
+    :doc "括弧のハイライト"
+    :custom ((show-paren-mode . t)
+             (show-paren-style . 'expression)
+             ))
+  )
+
+(leaf *global-set-key
+  :bind (("C-h" . delete-backward-char)
+         ("C-x C-b" . bs-show)
+         ("C-x C-r" . recentf)
+         ))
+
+(setq org-directory "~/Documents/org-mode")
+(setq org-default-notes-file "notes.org")
+(setq org-capture-templates
+      '(("n" "Note" entry (file+headline "~/Documents/org-mode/notes.org" "Notes")
+         "* %?\nEntered on %U\n %i\n %a")
+        ))
+
 (leaf ddskk
+  :doc "input method"
   :ensure t
-  :custom (
-           (skk-large-jisyo . "~/.emacs.d/skk-get-jisyo/SKK-JISYO.L")
+  :custom ((skk-large-jisyo . "~/.emacs.d/skk-get-jisyo/SKK-JISYO.L")
            (skk-japanese-message-and-error . t)
            (skk-status-indicator . 'minor_mode)
            (default-input-method . "japanese-skk")
-           )
-  )
+           ))
 
 (leaf mwim
   :ensure t
-  :bind(
-        ("C-a" . mwim-beginning)
-        ("C-e" . mwim-end)
-        )
-  )
+  :bind (("C-a" . mwim-beginning)
+         ("C-e" . mwim-end)
+         ))
 
-
-(leaf counsel
-  :doc "include ivy swipper"
+(leaf vertico
   :ensure t
-  :custom (
-           (ivy-wrap . t)
-           (ivy-mode . t)
-           )
-  :bind (
-         ("C-s" . swiper)
-         ("C-x C-b" . ivy-switch-buffer)
-         ("C-x C-f" . counsel-find-file)
-         ("C-x C-r" . counsel-recentf)
-         ("M-x" . counsel-M-x)
-         ("M-y" . counsel-yank-pop)
-         )
-  :config
-  (leaf ivy-posframe
-    :ensure t
-    :after ivy posframe
-    :custom (
-             (ivy-posframe-display-functions-alist . '(
-                                                       (counsel-find-file . ivy-posframe-display-at-point)
-                                                       (counsel-M-x . ivy-posframe-display-at-point)
-                                                       (counsel-recentf . ivy-posframe-display-at-point)
-                                                       (counsel-yank-pop . ivy-posframe-display-at-point)
-
-                                                       (ivy-switch-buffer . ivy-posframe-display-at-point)
-                                                       (t . ivy-posframe-display-at-frame-bottom-window-center)
-                                                       )
-                                                   )
-             (ivy-posframe-mode . t)
-             )
-    )
-  (leaf ivy-rich
-    :ensure t
-    :custom (
-             (ivy-rich-mode . t)
-             )
-   )
+  :global-minor-mode vertico-mode
   )
+
+(leaf consult
+  :ensure t
+  :bind (("C-s" . consult-line))
+  )
+
+(leaf orderless
+  :ensure t
+  )
+(setq completion-styles '(orderless basic)
+      completion-category-overrides '((file (styles basic partial-completion))))
+(leaf marginalia
+  :ensure t
+  :global-minor-mode marginalia-mode)
+
+(leaf embark
+  :disabled t
+  :bind (("s-a" . embark-act)))
+
+
+
+;; (leaf counsel
+;;   :ensure t
+;;   :bind (("C-s" . swiper-isearch)
+;;          ("C-x C-b" . counsel-switch-buffer)
+;;          ("C-x C-f" . counsel-find-file)
+;;          ("C-x C-r" . counsel-recentf)
+;;          ("M-x" . counsel-M-x)
+;;          ("M-y" . counsel-yank-pop)
+;;          )
+;;   :configq
+;;   (leaf ivy-rich
+;;     :ensure t
+;;     :global-minor-mode t
+;;     )
+;;   )
 
 (leaf company
-  :doc ""
+  :doc "入力補完UI"
   :ensure t
+  :global-minor-mode global-company-mode
   :custom (
-           (global-company-mode . t)
            (company-idle-delay . 0)
            (company-minimum-prefix-length . 2)
            (company-selection-wrap-around . t)
            (company-dabbrev-downcase . nil)
            )
-  :bind (
-         (:company-active-map
-          ("C-n" . company-select-next)
-          ("C-p" . company-select-previous)
+  :bind ((:company-active-map
           ("C-h" . nil)
-          )
-         )
+          ))
   :config
   (leaf company-posframe
+    :global-minor-mode t
     :ensure t
-    :after company posframe
-    :custom (
-             (require 'subr-x)
-             (company-posframe-mode . t)
-             (company-posframe-show-indicator . nil)
-             (company-posframe-show-metadata . nil)
-             (company-posframe-quickhelp-delay . nil)
-             )
     )
+  (leaf company-quickhelp
+
+    :ensure t
+  )
   )
 
-
-(leaf js2-mode
+(leaf lsp-mode
   :ensure t
-  )
+  :hook ((rust-mode-hook . lsp-deferred)
+         )
+  :custom (lsp-completion-provider . :none)
+  :config
+  (leaf lsp-ui
+    :ensure t
+    )
+  (with-eval-after-load 'lsp-mode
+    (add-to-list 'lsp-language-id-configuration
+                 '(qml-mode . "qmlls6"))
 
-(leaf php-mode
-  :ensure t
+    (message "%s" lsp-language-id-configuration)
+
+    (lsp-register-client
+     (make-lsp-client :new-connection (lsp-stdio-connection '("qmlls6" "--build-dir" "build/debug"))
+                      :activation-fn (lsp-activate-on "qmlls6")
+                      :server-id 'qmlls))
+    )
+(setq lsp-clients-clangd-args
+    '("--header-insertion=never" "--compile-commands-dir=build/debug"))
   )
+(setf lsp-prefer-capf t)
+(leaf rust-mode
+  :ensure t)
 
 (leaf web-mode
-  :ensure t
-  )
+  :ensure t)
 
-(leaf cmake-mode
+(leaf tree-sitter
   :ensure t
-  :disabled t
-  )
+  :config
+  (leaf tree-sitter-langs
+    :ensure t))
+
+(leaf editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode t))
+
+(require 'ansi-color)
+(add-hook 'compilation-filter-hook
+          '(lambda ()
+             (ansi-color-apply-on-region (point-min) (point-max))))
+
+(setq js2-strict-missing-semi-warning nil)
+(setq js2-missing-semi-one-line-override nil)
+
+(yas-global-mode t)
+(put 'dired-find-alternate-file 'disabled nil)
